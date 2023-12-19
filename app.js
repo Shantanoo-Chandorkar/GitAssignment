393f6aeba64c65ca608f7c93f3ca90846a60d1fe
Shantanoo <chandorkarshantanoo@gmail.com>
2023-12-19 14:15:44 +0530

    Added some elements and styling

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const h1 = document.querySelector("h1");
 console.log(h1);
 const p = document.querySelector("p");
 console.log("p: ", p);
+
+p.style.color = "red";
+p.style.backgroundColor = "cyan";
